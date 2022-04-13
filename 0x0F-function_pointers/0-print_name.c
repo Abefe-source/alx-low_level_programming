1fdd5f61ebeb50a82e4855d6983c50e239a3adc7
Abefe-source <oladegaadeola@gmail.com>
2022-04-13 12:38:16 -0700

    print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
-*print_mame_as_is- prints a name as is
-*@name: name of the person
-*
-*return: nothing.
-*/
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-
-                    if(name && f)
-       			f(name);
+	if (name && f)
+		f(name);
 }
