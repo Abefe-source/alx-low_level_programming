7306c4023b20f2b672a7fd8f4c773fa45d16cd4f
Abefe-source <oladegaadeola@gmail.com>
2022-04-28 16:58:56 -0700

    bitwise manipulations

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,31 +1,22 @@
 #include "main.h"
+
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
- * @n: is an unsigned long that will have a bit changed to one at index.
- * @index: is the bit to change 0 numbering.
- * Return: 1 or 0 or -1 if error.
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i, num, temp;
+	unsigned int m;
 
-	if (n == NULL || index > 32)
+	if (index > 63)
 		return (-1);
 
-	num = 1;
-	i = 0;
+	m = 1 << index;
+	*n = (*n | m);
 
-	while (i <= index)
-	{
-		if (i > 0)
-			num *= 2;
-		++i;
-	}
-	*n = *n | num;
-	temp = *n;
-	temp &= num;
-	if (temp == num)
-		return (1);
-	else
-		return (-1);
+	return (1);
 }
